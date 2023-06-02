9cd6176fd5145f3a15ac9ad091f6d9e869244f65
Subarna Karki <subarna.karki@infiniteblue.com>
2023-06-01 23:52:05 -0400

    add key to card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,12 +3,10 @@ import './card.styles.css';
 const Card = ({ monster }) => {
   const { name, email, id } = monster;
   return (
-    <div key={id}>
-      <div className="card-container">
-        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set8&size=180x180`} />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+    <div className="card-container">
+      <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set8&size=180x180`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
   );
 };
